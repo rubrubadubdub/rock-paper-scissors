8552169ec41f8ea35110b46823a3dff1b2a0b66f
Rob R <rs.romaine@gmail.com>
2022-05-20 22:36:07 +0900

    made prelim css and js changes

--- main.js
+++ main.js
@@ -1,32 +1,42 @@
-function computerPlay(){
+//let playerAnswer = parseInt(prompt("Please enter 1:Rock, 2:Paper, 3:Scissors"));
+let pcAnswer = computerPlay();
+let pcWrite = stringifyAns(pcAnswer);
+let playerWrite = stringifyAns(playerAnswer);
+let winner = decideWinner(playerWrite, pcWrite);
+let $result = 'The PC played ' + pcWrite + '. You played ' + playerWrite + '. So the winner is ' + winner + '.';
+console.log($result);
+
+
+
+function computerPlay() {
     return Math.floor(Math.random() * 3) + 1;
 }
-function decideWinner(player, pc){
+function decideWinner(player, pc) {
     let $result = new String();
-    if(player === pc){return "neither. It is a tie"}
-    
-    if(player == "rock"){
-        if(pc == "paper"){return "the PC"}
-        if(pc == "scissors"){return "the Player"}
+    if (player === pc) { return "neither. It is a tie" }
+
+    if (player == "rock") {
+        if (pc == "paper") { return "the PC" }
+        if (pc == "scissors") { return "the Player" }
     }
-    if(player == "paper"){
-        if(pc == "scissors"){return "the PC"}
-        if(pc == "rock"){return "the Player"}
+    if (player == "paper") {
+        if (pc == "scissors") { return "the PC" }
+        if (pc == "rock") { return "the Player" }
     }
-    if(player == "scissors"){
-        if(pc == "rock"){return "the PC"}
-        if(pc == "paper"){return "the Player"}
+    if (player == "scissors") {
+        if (pc == "rock") { return "the PC" }
+        if (pc == "paper") { return "the Player" }
     }
 
 }
 
-function stringifyAns(ans){
+function stringifyAns(ans) {
     let rock = "rock";
     let paper = "paper";
     let scissors = "scissors";
     ans = Number(ans);
-    if(ans == 1){return rock;}
-    if(ans == 2){return paper;}
-    if(ans == 3){return scissors;}
+    if (ans == 1) { return rock; }
+    if (ans == 2) { return paper; }
+    if (ans == 3) { return scissors; }
     return null;
 }
